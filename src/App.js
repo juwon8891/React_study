372270b087318038d965be3db3e874e852a4f4e5
kevin <juwon8891@gmail.com>
2022-05-06 18:19:00 +0900

    리액트

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let post = '강남 우동 맛집';
+  let [글제목,setValue] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬독학']);
+  let styles = { color : 'orange', fontSize : '20px' };
+  let date = '5월 6일 발행';
+  let [따봉, 따봉변경] = useState(0);
+
   return (
-    <div classNcame="App">
-      <div classNcame="black-nav">
-      <h4>블로그임</h4>
+    <div className="App">
+      <div className="black-nav">
+      <h4 style= { styles }>ReactBlog</h4>
+      </div>
+      <div>
+      <div>
+      <div className="list">
+        <h4>{글제목} <span onClick={ ()=>{따봉변경(따봉++)}   }>좋아요😊</span> {따봉} </h4>
+        <p>{date}</p>
+        </div>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>{date}</p>
+      </div>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>{date}</p>
+      </div>
+      </div>
       </div>
     </div>
   );
